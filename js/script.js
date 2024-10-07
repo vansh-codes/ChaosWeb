078626cd4df27a1e6240fe582264e00b6947410d
candy <m001720@permatapintar.ukm.edu.my>
2024-10-07 17:12:33 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.getElementById('chaos-header');
+  const words = header.textContent.split(' ');
+  header.innerHTML = words.map(word => `<span>${word}</span>`).join(' ');
+});
